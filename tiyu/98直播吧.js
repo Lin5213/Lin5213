1bceea0eed432a6dfba12dd3fb058e337cb1d1ba
zxwwp <zxwwp@163.com>
2023-09-15 07:34:20 +0000

    更新 'tiyu/98直播吧.js'

--- tiyu/98直播吧.js
+++ tiyu/98直播吧.js
@@ -4,14 +4,6 @@ var rule = {
     url:'/fyclass',
     class_name:'赛事直播✨注意时间',       //静态分类名称拼接
     class_url:'/',    //静态分类标识拼接
-    headers:{
-        'User-Agent':'MOBILE_UA'
-    },
-    timeout:5000,
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    double:false,
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.live_list_content li');tabs.forEach(function(it){var pz=pdfh(it,'td:eq(0)&&Text');var ps=pdfh(it,'strong&&Text');var pk=pdfh(it,'td:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.s1_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{

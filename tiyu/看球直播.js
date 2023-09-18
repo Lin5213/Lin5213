c684c6199be82c3d95000ebda6561c536fa4e0c7
zxwwp <zxwwp@163.com>
2023-09-18 00:54:14 +0000

    更新 'tiyu/看球直播.js'

--- tiyu/看球直播.js
+++ tiyu/看球直播.js
@@ -1,9 +1,9 @@
 var rule = {
     title:'看球直播',
-    host:'https://kanqiu01.com/schedule/lists',
+    host:'https://kanqiu01.com/',
     url:'/fyclass',
     class_name:'赛事直播✨注意时间',       //静态分类名称拼接
-    class_url:'/',    //静态分类标识拼接
+    class_url:'schedule/lists',    //静态分类标识拼接
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.schedule_detail_box li');tabs.forEach(function(it){var pz=pdfh(it,'.team_home span&&Text');var ps=pdfh(it,'span.aliases&&Text');var pk=pdfh(it,'.tema_away span&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.timer&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{

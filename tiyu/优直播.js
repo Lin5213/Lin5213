493f16508fe402331561b5eb983f4ea46a9c4601
zxwwp <zxwwp@163.com>
2023-10-07 01:45:00 +0000

    更新 'tiyu/优直播.js'

--- tiyu/优直播.js
+++ tiyu/优直播.js
@@ -1,14 +1,12 @@
 var rule = {
     title:'优直播',
-    host:'https://www.yoozhibo.net/{cateId}',
+    host:'https://www.yoozhibo.net/',
     url:'/fyclass',     //网站的分类页面链接
     class_name:'全部&足球&篮球&综合',       //静态分类名称拼接
     class_url:'/&zuqiu&lanqiu&zonghe',    //静态分类标识拼接
     homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
     推荐:'*',
-      // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&ul.item.cd');tabs.forEach(function(it){var  pz=pdfh(it,'.name:eq(0)&&Text');var ps=pdfh(it,'.league&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.time&&Text');var url=pd(it,'li.play a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
-    // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
      二级:{
           title:'.team b&&Text;.sig-vs-txt b&&Text',  //片名;类型 时间
           img:"img&&src",

a505bc3c4979f56017b4e01bd7ece173dd913b6d
zxwwp <zxwwp@163.com>
2023-10-07 01:27:56 +0000

    更新 'tiyu/310直播.js'

--- tiyu/310直播.js
+++ tiyu/310直播.js
@@ -16,9 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    //一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
-     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.list_content a');tabs.forEach(function(it){var pz=pdfh(it,'.zhudui_wuyd&&Text');var ps=pdfh(it,'.diss&&Text');var pk=pdfh(it,'.team.kedui&&Text');var img=pd(it,'img&&op-zfr-a-g');var timer=pdfh(it,'p.eventtime&&Text');var url=pd(it,'a.clearfix.zhiding a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
-    
+    一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
     二级:'*', 
     搜索:'',
 }
